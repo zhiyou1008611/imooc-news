1a550e747c70a00654ef8179338ff435db090889
easicare--web <easicareweb@easicare.com>
2021-03-10 10:08:00 +0800

    首次提交

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="navbar">
+		<view class="navbar-serach">
+			<view class="navbar-serach_icon"></view>
+			<view class="navbar-serach_text"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss">
+.navbar {
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 99;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0 15px;
+	width: 100%;
+	height: 45px;
+	background-color: $mk-base-color;
+	box-sizing: border-box;
+	.navbar-serach {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		height: 30px;
+		padding: 0 10px;
+		border-radius: 15px;
+		background-color: #ffffff;
+		.navbar-serach_icon {
+			width: 10px;
+			height: 10px;
+			border: 1px solid red;
+			margin-right: 10px;
+		}
+		.navbar-serach_text {
+			font-size: 12px;
+			color: #999999;
+		}
+	}
+}
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		<!-- 自定义组件 -->
+		<navbar></navbar>
+		<view class="" v-for="item in 50">
+			{{item}}
+		</view>
+	</view>
+</template>
+
+<script>
+import navbar from '../../../components/navbar/navbar.vue';
+export default {
+	components: { navbar },
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss"></style>
